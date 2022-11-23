6b3476339adeb5bfb4a3114a50848a32476f41cc
Sarwar Azmain Eyafi <75948244+EyafiNeo@users.noreply.github.com>
2022-11-24 01:58:22 +0600

    Categories check

--- index.js
+++ index.js
@@ -9,6 +9,33 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bz2bbta.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+
+        const categoriesCollection = client.db("boilagbedb").collection("categories");
+
+        app.get('/admin/categories', async(req,res)=>{
+            const query = {};
+            const cursor = categoriesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+    }finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Boilagbe server running!')
